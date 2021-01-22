8d935811f11fd15f03a15db86ca235789902b700
Naveen B Jacob <naveenbjacob123@gmail.com>
2021-01-22 21:58:25 +0530

    Updates

---
+++ precache-manifest.5c3078d267ca7b3a615282a5fd925833.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a7f91641b55ab8ac3ed1321f7762ac",
+    "url": "/acm-webpage/index.html"
+  },
+  {
+    "revision": "5439469f7dae899da549",
+    "url": "/acm-webpage/static/css/2.8d0ed72a.chunk.css"
+  },
+  {
+    "revision": "22d788901b610950545f",
+    "url": "/acm-webpage/static/css/main.807db7fc.chunk.css"
+  },
+  {
+    "revision": "5439469f7dae899da549",
+    "url": "/acm-webpage/static/js/2.6a9e47f6.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/acm-webpage/static/js/2.6a9e47f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d788901b610950545f",
+    "url": "/acm-webpage/static/js/main.8332b05c.chunk.js"
+  },
+  {
+    "revision": "a8e12203c1a54147e16f",
+    "url": "/acm-webpage/static/js/runtime-main.6a36c042.js"
+  },
+  {
+    "revision": "d9fd69237530a10c718002e52af5bc4f",
+    "url": "/acm-webpage/static/media/Acmlogo.d9fd6923.jpeg"
+  },
+  {
+    "revision": "6d7fe3620eb0abb205f360ec33f59e8c",
+    "url": "/acm-webpage/static/media/logo.6d7fe362.png"
+  }
+]);
